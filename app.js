e9ed40589d21af15ea49123182df4944ea2b3c66
Alexander_Ovchar <azalexxx02@gmail.com>
2025-11-07 01:37:52 +0200

    Implement file change watcher for command.txt and read its content on changes

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const fs = require('fs/promises');
 
 (async () => {
-  const watcher = fs.watch("./");
+  const commandFileHandler = await fs.open("./command.txt", "r");
+
+  commandFileHandler.on("change", async () => {
+    const size = (await commandFileHandler.stat()).size;
+    const buffer = Buffer.alloc(size);
+
+    const offset = 0;
+    const length = buffer.byteLength;
+    const position = 0;
+
+    const content = await commandFileHandler.read(buffer, offset, length, position);
+    console.log(content);
+  })
+
+  const watcher = fs.watch("./command.txt");
+
   for await (const event of watcher) {
-    console.log(event);
+    if (event.eventType === "change") {
+      commandFileHandler.emit("change");
+    }
   }
 })();
