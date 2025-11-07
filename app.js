b93a1b8d9408510634d196aa34754570ce195410
Alexander_Ovchar <azalexxx02@gmail.com>
2025-11-07 02:01:12 +0200

    Add createFile function to handle file creation from command input

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const fs = require('fs/promises');
 
 (async () => {
+
+  const createFile = async (filePath) => {
+    try {
+      const existingFileHandler = await fs.open(filePath, "r");
+      existingFileHandler.close();
+      return console.log(`The file ${filePath} already exists`);
+    } catch (error) {
+      const newFileHandler = await fs.open(filePath, "w");
+      console.log("A new file was successfully created");
+      newFileHandler.close();
+    }
+
+  }
+
+  const CREATE_FILE = "create a file";
+
   const commandFileHandler = await fs.open("./command.txt", "r");
 
   commandFileHandler.on("change", async () => {
@@ -11,8 +27,14 @@ const fs = require('fs/promises');
     const length = buffer.byteLength;
     const position = 0;
 
-    const content = await commandFileHandler.read(buffer, offset, length, position);
-    console.log(content);
+    await commandFileHandler.read(buffer, offset, length, position);
+
+    const command = buffer.toString("utf-8")
+
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE.length + 1)
+      createFile(filePath);
+    }
   })
 
   const watcher = fs.watch("./command.txt");
