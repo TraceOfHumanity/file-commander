820e7fd26f8116f9551556c1f2db81bf609d538d
Alexander_Ovchar <azalexxx02@gmail.com>
2025-11-07 21:23:50 +0200

    Add file handling functions for deletion, renaming, and content addition based on command input

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fs = require('fs/promises');
 
 (async () => {
+  const CREATE_FILE = "create a file";
+  const DELETE_FILE = "delete a file";
+  const RENAME_FILE = "rename the file";
+  const ADD_TO_FILE = "add to file";
 
   const createFile = async (filePath) => {
     try {
@@ -12,10 +16,19 @@ const fs = require('fs/promises');
       console.log("A new file was successfully created");
       newFileHandler.close();
     }
-
   }
 
-  const CREATE_FILE = "create a file";
+  const deleteFile = async (filePath) => {
+    console.log(`The file ${filePath} was successfully deleted`);
+  }
+
+  const renameFile = async (filePath, newFilePath) => {
+    console.log(`The file ${filePath} was successfully renamed to ${newFilePath}`);
+  }
+
+  const addToFile = async (filePath, content) => {
+    console.log(`The file ${filePath} was successfully updated with the content: ${content}`);
+  }
 
   const commandFileHandler = await fs.open("./command.txt", "r");
 
@@ -34,6 +47,25 @@ const fs = require('fs/promises');
     if (command.includes(CREATE_FILE)) {
       const filePath = command.substring(CREATE_FILE.length + 1)
       createFile(filePath);
+    }
+
+    if (command.includes(DELETE_FILE)) {
+      const filePath = command.substring(DELETE_FILE.length + 1)
+      deleteFile(filePath);
+    }
+
+    if (command.includes(RENAME_FILE)) {
+      const _idx = command.indexOf(" to ");
+      const oldFilePath = command.substring(RENAME_FILE.length + 1, _idx);
+      const newFilePath = command.substring(_idx + 4);
+      renameFile(oldFilePath, newFilePath);
+    }
+
+    if (command.includes(ADD_TO_FILE)) {
+      const _idx = command.indexOf(" this content: ");
+      const filePath = command.substring(ADD_TO_FILE.length + 1, _idx);
+      const content = command.substring(_idx + 15);
+      addToFile(filePath, content);
     }
   })
 
